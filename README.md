17c65929b7f21fa32282a36c3af9def208cda1cc
ignatas <40527112+ignatas@users.noreply.github.com>
2020-05-02 00:22:52 +0300

    2

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # discord-bd-boss-bot
- 
--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const Discord = require('discord.js'); 
-const bot = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 //подключаем файл конфигурации
-let config = require('/botconfig.json'); 
-let token = config.token; 
+let config = require('./botconfig.json');
+let boss = require('./bosshelper.json');
+let token = config.token;
 let prefix = config.prefix;
 
 //создаём ссылку-приглашение для бота
-bot.on('ready', () => { 
-    console.log(`Привет! Я ${bot.user.username}`);
-    bot.generateInvite(["ADMINISTRATOR"]).then(link => { 
+client.on('ready', () => {
+    console.log(`Запустился бот ${client.user.username}`);
+    client.generateInvite(["ADMINISTRATOR"]).then(link => {
         console.log(link);
     });
 });
 //команда, и то, что она должна выполнить
-bot.on('message', msg => {
+client.on('message', msg => {
     if (msg.content === prefix + 'Пафнутий') {
         msg.reply('Внемлю твоему зову');
     }
 });
-bot.login(token);
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Не гневи небо');
+    }
+});
+client.on('message', msg => {
+    if (msg.content === prefix + 'босс') {
+        let now = new Date(Date.now());
+        let day = now.getDay();        
+        let hour = now.getHours() + 1;
+        if (hour == '24') { day = day + 1; hour = '0' }
+        if (day == '7') { day = '0' }
+        console.log(day)
+        console.log(hour)
+        msg.reply(`Вскоре восстанет ${boss[day][hour]}! К оружию, браться и сестры`);
+    }
+});
+
+client.login(token);
